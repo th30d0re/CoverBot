8d82b72ca2b4a716ace3b45a72a2d85572f6b9c0
Emmanuel Theodore <99168111+th30d0re@users.noreply.github.com>
2023-05-10 15:35:32 -0400

    Initial Commit

---
+++ CoverBot/CoverBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverBotApp.swift
+//  CoverBot
+//
+//  Created by Emmanuel Theodore on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoverBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
